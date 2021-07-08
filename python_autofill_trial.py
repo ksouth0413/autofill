2c5c093c5ff6310dc121165b2c3b5b32e666dc81
ksouth0413 <68626299+ksouth0413@users.noreply.github.com>
2021-07-08 16:42:30 +0900

    Update python_autofill_trial.py

--- python_autofill_trial.py
+++ python_autofill_trial.py
@@ -2,7 +2,17 @@ from time import sleep
 from selenium import webdriver
 
 #instead of this >> driver = webdriver.Chrome('/Applications/chromedriver')
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("lang=ko_KR")
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument("--no-sandbox")
+
 driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver.implicitly_wait(3)
+
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSeEtkMWe9eKQb6tMSYlLpE1YefDm4fDptLdwvk6Pqifr9ESPw/viewform'
 driver.get(url)
 
